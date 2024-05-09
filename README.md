c1efedce4acc2bcfc6c3643241a3329370f4dd3b
Roman Burgoch <r.burgoch@yandex.ru>
2024-05-09 12:41:18 +0300

    Проектная работа №5 готова

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/RomanBurgoch/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function renderCard (item, removeCard) {
+    const placesItemClone = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const cardImage = placesItemClone.querySelector('.card__image');
+    const cardTitle = placesItemClone.querySelector('.card__description');
+    const deleteButton = placesItemClone.querySelector('.card__delete-button');
+    cardImage.src = item.link;
+    cardImage.alt = item.name;
+    cardTitle.textContent = item.name;
+    placesList.append(placesItemClone);
+    deleteButton.addEventListener('click', (removeCard));
+}
 
 // @todo: Функция удаления карточки
+function removeCard (event) {
+    event.target.parentElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(item) {
+    renderCard(item, removeCard);
+})
+
+
+
